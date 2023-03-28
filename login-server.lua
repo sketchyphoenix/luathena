80dba5e4744f117ea23a456c46e8f6aa41de3838
sketchyphoenix <sketchyphoenix@gmail.com>
2023-03-28 06:39:12 -0500

    Dropped coroutine handler

--- login-server.lua
+++ login-server.lua
@@ -2,13 +2,37 @@ local core = require "src/common/core"
 local loginserver = require "config/login_config"
 
 local client_table = {}
-local threads = {}
 local socket = require("socket")
 local server = socket.bind(loginserver.bind_ip, loginserver.port)
 
 while 1 do
     local recvt = socket.select(client_table, nil, 1)
-    local client = server:accept()
+
+    for _,client in ipairs(recvt) do
+        local data, status = core:ReadSocket(client)
+        data = core:Handler(data)
+        if status == "closed" then 
+            core:RemoveClientFromTable(client, client_table)
+        end
+    end
+
+    local connection = server:accept()
+    if connection then
+        core:AddClientToTable(connection, client_table)
+    end
+
+
+    --[[
+
+    local client = recvt:accept()
+
+    if client then
+        core:AddClientToTable(client, client_table)
+    end
+
+    for _,cl in ipairs(recvt) do
+        print(_) print(cl)
+    end
 
     local co = coroutine.create(function (client)
         client:settimeout(0.1)
@@ -25,11 +49,6 @@ while 1 do
         return data, status
     end)
 
-    if client then
-        core:AddClientToTable(client, client_table)
-        table.insert(threads,co)
-    end
-
     for i=1,#threads do
         local coroutine_status, data, socket_status = coroutine.resume(threads[i], client)
         if socket_status == "closed" or coroutine_status == false then
@@ -39,6 +58,8 @@ while 1 do
         local data_out = core:Handler(data)
         client:send(data_out)
     end
+
+    ]]
 
     --client:close()
 end
--- src/common/core.lua
+++ src/common/core.lua
@@ -10,6 +10,7 @@ end
 function core:AddClientToTable(client, client_table)
     for index,value in ipairs(client_table) do
         if value == client then
+            print('already here')
             return client
         end
     end
@@ -18,18 +19,40 @@ function core:AddClientToTable(client, client_table)
     return client
 end
 
+function core:RemoveClientFromTable(client, client_table)
+    for index,value in ipairs(client_table) do
+        if value == client then
+            client_table[index] = nil
+        end
+    end
+    return client_table[index]
+end
+
+function core:ReadSocket(client)
+    client:settimeout(0.1)
+    local data, status, partial = client:receive(65535)
+    
+    if partial then
+        data = partial
+    end
+
+    return data, status
+end
+
 function core:Handler(data)
     local code = string.unpack("<i2", data)
 
     if code == 0x2710 then return core:ParseCharConnectRequest(data) end
+
+    return nil
 end
 
 --[[
    0x2710
 ]]
 function core:ParseCharConnectRequest(data)
-    local inter_username = string.sub(data,3,26) print(inter_username)
-    local inter_password = string.sub(data,27,50) print(inter_password)
+    local inter_username = string.sub(data,3,26)
+    local inter_password = string.sub(data,27,50)
 
     local ip_byte_array = string.unpack(">i4", string.sub(data,55,58))
     local ipaddr = tonumber(ip_byte_array)
@@ -38,12 +61,11 @@ function core:ParseCharConnectRequest(data)
     local oct3 = (ipaddr >> 8) & 0xFF
     local oct4 = (ipaddr >> 0) & 0xFF
     ipaddr = oct1.."."..oct2.."."..oct3.."."..oct4
-    print (ipaddr)
 
-    local port = string.unpack(">i2", string.sub(data,59,60)) print(port)
-    local server_name = string.sub(data,61,80) print(server_name)
-    local maintenance_flag = string.unpack("<i1", string.sub(data,83)) print(maintenance_flag)
-    local display_new_flag = string.unpack("<i1", string.sub(data,85)) print(display_new_flag)
+    local port = string.unpack(">i2", string.sub(data,59,60))
+    local server_name = string.sub(data,61,80)
+    local maintenance_flag = string.unpack("<i1", string.sub(data,83))
+    local display_new_flag = string.unpack("<i1", string.sub(data,85))
 
     local code = string.pack("<i2", 0x2711)
     local status = string.pack("<i1", 0)
